f556b68b1300498b51b8c17ed6b0dd3100689b18
proshchyna <pavelrosh@gmail.com>
2020-12-03 15:42:30 +0200

    added IAM role and profile creation

--- configs/configure_ec2_environment.sh
+++ configs/configure_ec2_environment.sh
@@ -1,7 +1,8 @@
 sudo yum -y install python3
 sudo yum -y install git
 
-git clone https://github.com/proshchyna/proshchyna_amazon_bigdata_capstone.git
+cd /var/run && rm -f yum.pid
+cd /home/ec2-user && git clone https://github.com/proshchyna/proshchyna_amazon_bigdata_capstone.git
 python3 -m venv proshchyna_amazon_bigdata_capstone/venv
 source proshchyna_amazon_bigdata_capstone/venv/bin/activate && pip install -r proshchyna_amazon_bigdata_capstone/requirements.txt
 #set -a; source .env; set +a
