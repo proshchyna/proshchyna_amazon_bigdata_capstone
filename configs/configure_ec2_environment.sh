f08927a60c4e355c92c86f356f03a342a473df7d
proshchyna <pavelrosh@gmail.com>
2020-12-03 19:00:50 +0200

    added kinesis data stream

--- configs/configure_ec2_environment.sh
+++ configs/configure_ec2_environment.sh
@@ -7,6 +7,8 @@ cd /home/ec2-user && git clone https://github.com/proshchyna/proshchyna_amazon_b
 python3 -m venv proshchyna_amazon_bigdata_capstone/venv
 source proshchyna_amazon_bigdata_capstone/venv/bin/activate && pip install -r proshchyna_amazon_bigdata_capstone/requirements.txt
 #set -a; source .env; set +a
+
+sudo mkdir /tmp/capstone/ && sudo mkdir /tmp/capstone/views && sudo mkdir /tmp/capstone/reviews
 source proshchyna_amazon_bigdata_capstone/venv/bin/activate && cd proshchyna_amazon_bigdata_capstone/ && python data_generator.py
 
 # install and set up kinesis agent
--- configs/settings.py
+++ configs/settings.py
@@ -3,10 +3,3 @@ import os
 
 dotenv_path = 'configs/.env'
 load_dotenv(dotenv_path)
-
-try:
-	if not os.path.exists('/opt/capstone/views') or not os.path.exists('/opt/capstone/reviews'):
-		os.makedirs('/opt/capstone/reviews')
-		os.makedirs('/opt/capstone/views')
-except:
-	pass
